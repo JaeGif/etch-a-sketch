4bdd97a6f185cee21fd7a0e5ac40f282c5ccd0e5
Jae Gifford <jae.giff@outlook.com>
2022-06-16 18:08:57 +0800

    grid now updates according to sie choice. makes the correct number of divs

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gridSource = document.querySelector('#grid-container')
 const clearButtonSource = document.querySelector('#clear')
 const aspectSliderSource = document.querySelector('#slider')
-let gridDivs = document.createElement('div')
 let colorChoice = document.getElementById("color-picker");
 
+console.log((aspectSliderSource.ariaValueMax))
 // make a 16x16 grid first
 // biggest 8x8, smallest ratio 128x128
-
-for (let i=0; i <16; i++){
-    gridSource.appendChild(gridDivs)
+function setGrid(size) {
+    gridSource.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridSource.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    for (let i=0; i < (size*size); i++) {
+        const gridDivs = document.createElement('div')
+        //gridDivs.addEventListener('mouseover', /* chage color fn */)
+        //gridDivs.addEventListener('mousedown', /* chage color fn */)
+        gridSource.appendChild(gridDivs)
+    }
 }
+
+function sizeChange() {
+    // change size of grid
+}
+
+setGrid(128)
+
