af4f025cc2797142958c25d3577e7e3a6c369440
Jae Gifford <jae.giff@outlook.com>
2022-06-16 17:28:22 +0800

    added div making logic

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const gridSource = document.querySelector('#grid-container')
 const clearButtonSource = document.querySelector('#clear')
 const aspectSliderSource = document.querySelector('#slider')
-
+let gridDivs = document.createElement('div')
 let colorChoice = document.getElementById("color-picker");
+
+// make a 16x16 grid first
+// biggest 8x8, smallest ratio 128x128
+
+for (let i=0; i <16; i++){
+    gridSource.appendChild(gridDivs)
+}
