4e86bc764276c9c47109fc4f61d5b26280487415
Jae Gifford <jae.giff@outlook.com>
2022-06-16 15:17:52 +0800

    added a color picker element

---
+++ script.js
@@ -0,0 +1,5 @@
+const gridSource = document.querySelector('#grid-container')
+const clearButtonSource = document.querySelector('#clear')
+const aspectSliderSource = document.querySelector('#slider')
+
+let colorChoice = document.getElementById("color-picker");
