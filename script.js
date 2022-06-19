663779df982f182382f6ad2044a6344b1e489bf5
Jae Gifford <jae.giff@outlook.com>
2022-06-19 12:59:07 +0800

    cleanup and comments added

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
+// assign defaults
 const DEFAULT_MODE = 'color'
 const DEFAULT_SIZE = 8
 
+// assign defaults to the mutable variables on page load 
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
 
+// Page Element selection constants
 const gridSource = document.querySelector('#grid-container')
 const clearButtonSource = document.querySelector('#clear')
 const aspectSliderSource = document.querySelector('#slider')
-aspectSliderSource.addEventListener('input', changeSize)    // listens on input slide, dynamically changing
-//var mouseDownCheck = false
+
+// permanent listeners
+aspectSliderSource.addEventListener('input', changeSize)    // listens for slider movement
+clearButtonSource.addEventListener('click', () => {     // clear on click
+    clearGrid()
+    loadDEFAULTS()
+    })
+
 
 //gridSource.addEventListener('mousedown', () => {mouseDownCheck = true})
 
 function setGrid(size) {
-    gridSource.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    gridSource.style.gridTemplateRows = `repeat(${size}, 1fr)`
-    for (let i=0; i < (size*size); i++) {
-        const gridDivs = document.createElement('div')
-        gridDivs.addEventListener('mousedown', changeColor)
-        
-        gridDivs.className = 'grid-children'
-        //gridDivs.addEventListener('mousedown', /* chage color fn */)
+    gridSource.style.gridTemplateColumns = `repeat(${size}, 1fr)`       // set grid size (inline CSS)
+    gridSource.style.gridTemplateRows = `repeat(${size}, 1fr)`     
+    for (let i=0; i < (size*size); i++) {       // generate the grid elements based on size selection
+        const gridDivs = document.createElement('div')    
+        gridDivs.className = 'grid-children'        // assign class for CSS on-hover effects
+        gridDivs.addEventListener('click', changeColor)     // add an on click listener to change colors
         gridSource.appendChild(gridDivs)
-        //gridDivs.addEventListener('mouseover', changeColor())
 
     }
 }
 
 function changeColor(e) {
+    // select color using color swatch
     let colorChoice = document.getElementById("color-picker")
     e.target.style.backgroundColor = colorChoice.value
 
 }
 function changeSize() {
-    let currentSize = aspectSliderSource.value  //change size value based on slider
+    // change size via slider
+    currentSize = aspectSliderSource.value 
     clearGrid()     // clear grid for performance
     setGrid(currentSize)
 
 }
 
 function loadDEFAULTS() {  // initializes the minimum page requirements
-    setGrid(currentSize)
-    
+    setGrid(DEFAULT_SIZE)
+    aspectSliderSource.value = 8
 }
 
-function clearGrid() {      // removes element children
+function clearGrid() {      // removes element children, resets grid with current size selection
     while (gridSource.lastChild) {
         gridSource.removeChild(gridSource.lastChild);
     }
