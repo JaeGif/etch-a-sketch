8b41b43ec1af8667aaeea592f9c6447c1294ea03
Jae Gifford <jae.giff@outlook.com>
2022-06-19 13:32:08 +0800

    started mode functions, added charcoal mode

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // assign defaults
-const DEFAULT_MODE = 'color'
 const DEFAULT_SIZE = 8
-
+const DEFAULT_COLOR = '#000000'
+const DEFAULT_MODE = 'precise'
 // assign defaults to the mutable variables on page load 
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
+let currentChoice = DEFAULT_COLOR
 
 // Page Element selection constants
 const gridSource = document.querySelector('#grid-container')
@@ -30,14 +31,44 @@ function setGrid(size) {
     for (let i=0; i < (size*size); i++) {       // generate the grid elements based on size selection
         const gridDivs = document.createElement('div')    
         gridDivs.className = 'grid-children'        // assign class for CSS on-hover effects
-        gridDivs.addEventListener('click', changeColor)     // add an on click listener to change colors
+        changeMode(gridDivs)     // add an on click listener to change colors
         gridSource.appendChild(gridDivs)
 
     }
 }
+function changeMode(gridE) {
+    if (currentMode == 'precise') {
+        preciseMode(gridE);
+    } else if (currentMode == 'pen') {
+        penMode(gridE)
+    } else if (currentMode == 'rainbow') {
+        rainbowMode(gridE)
+    } else if (currentMode == 'charcoal') {
+        charcoalMode(gridE)
+    } else {
+        alert('Something has gone horribly wrong, please reload the page')
+    }
+}
+function preciseMode(preciseElements) {
+    preciseElements.addEventListener('click', changeColor)
+}
+function passOverListeners() {
+    // event listeners assigned if the element is passed over while the mouse is held down
+    let clickCheck = false
+}
+function penMode(penElements) {
+    // change color on mouse down until mouse down false
+}
+function rainbowMode(rainbowElements) {
+    // pen mode, change color randomly
+}
+function charcoalMode(charcoalElements) {
+    // pen mode make color a 10% darker on subsequent passes over an element
+
+}
 function changeColor(e) {
     // select color using color swatch
-    let colorChoice = document.getElementById("color-picker")
+    colorChoice = document.getElementById("color-picker")
     e.target.style.backgroundColor = colorChoice.value
 
 }
@@ -54,6 +85,8 @@ function loadCurrent() {
 }
 function loadDEFAULTS() {  // initializes the minimum page requirements
     currentSize = DEFAULT_SIZE
+    currentChoice = DEFAULT_COLOR
+    currentMode = DEFAULT_MODE
     setGrid(currentSize)
     aspectSliderSource.value = 8
 }
